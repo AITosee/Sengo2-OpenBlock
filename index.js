6e8606b7d01dae29fe060d3542895f817e751ff1
feng <succeed_happu@outlook.com>
2025-06-19 07:21:10 +0800

    fix(Sengo2): 更新版本号至 0.0.4 并修改标签为 'AI'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Sengo2 = formatMessage => ({
         description: 'name of Sengo2 vision sensor extension'
     }),
     extensionId: 'Sengo2',
-    version: '0.0.3',
+    version: '0.0.4',
     supportDevice: ['arduinoUno','arduinoUnoR4Wifi','arduinoUnoR4Minima', 'arduinoNano', 'arduinoLeonardo', 'arduinoMega2560', 
         'arduinoEsp32', 'arduinoEsp8266','arduinoEsp32S3',
         'arduinoK210','arduinoK210MaixDock','arduinoK210Maixduino','arduinoLeonardo',
@@ -23,7 +23,7 @@ const Sengo2 = formatMessage => ({
     toolbox: 'toolbox.js',
     translations: 'translations.js',
     library: 'lib',
-    tags: ['sensor', 'vision'],
+    tags: ['sensor', 'AI'],
     helpLink: 'https://tosee.readthedocs.io/zh/latest/'
 });
 
